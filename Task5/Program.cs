65ce2cc2e1da707efde19f1125af35cca9eecf1e
IchBinOktay <165468527+IchBinOktay@users.noreply.github.com>
2024-08-04 12:56:54 +0400

    Add files via upload

---
+++ Task5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Task5
+{
+
+
+    public static class Program
+    {
+        public static void Main()
+        {
+            Electronic machine = new Electronic("iphone",300,"silver");
+            Clothes clothes = new Clothes("nike", 50, "l");
+            Clothes clothes2 = new Clothes("balenciaga", 100, "S");
+            Order order = new Order();
+            order.AddProduct(machine);
+            order.AddProduct(clothes);
+            order.RemoveProduct(clothes);
+            order.GetDetails();
+            Console.WriteLine(order.GetTotalAmount());
+
+
+        }
+    }
+}
